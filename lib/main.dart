351a6e45c086879a712538ef800d8f898dd71ad7
Aftab Ali <aftabali553911@gmail.com>
2023-09-25 15:18:50 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:outsource/widgets/animated_container.dart';
+import 'package:outsource/widgets/animated_cross_fade.dart';
+import 'package:outsource/widgets/animated_opacity.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: AnimatedOpacityWidget(),
+    ),
+  ));
+}
+
